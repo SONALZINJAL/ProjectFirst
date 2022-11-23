ee9ea122cfef3873c5a2dd2df8c3ca4dab49851a
[200~https://github.com/SONALZINJAL~ <sonalzinjal90@gmail.com>
2022-11-23 21:19:40 +0530

    my second commit

--- ProjectFirst/src/com/velocity/java/project/Example.java
+++ ProjectFirst/src/com/velocity/java/project/Example.java
@@ -1,6 +1,8 @@
 package com.velocity.java.project;
-
+//Print duplicate element from arraylist
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Example {
 
@@ -10,7 +12,11 @@ public class Example {
 		name.add("sweety");
 		name.add("Amrita");
 		name.add("seema");
+		name.add("Amrita");
+		Set<String> set=new HashSet<>();
+		
 		for(String s:name) {
+			if (set.add(s)==false)  //my second commit on github
 			System.out.println(s);
 		}
 
